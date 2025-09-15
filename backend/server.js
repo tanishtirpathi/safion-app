39ee8e42db5d3a9813340c1d7436f535f87f044c
tanish tirpathi <tanishtirpathi0@gmail.com>
2025-09-15 23:12:10 +0530

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-
 dotenv.config();
 
 const authRoutes = require("./routes/auth");
@@ -14,16 +13,27 @@ const newsRoutes = require("./routes/news.routes");
 
 const chatbotRoute = require("./routes/chatbot");
 const app = express();
-app.use(cors());
+const allowed = ["http://localhost:5000", "https://safion.vercel.app"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // mobile apps / curl
+      if (allowed.indexOf(origin) !== -1) return callback(null, true);
+      return callback(new Error("CORS not allowed"), false);
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/news", newsRoutes);
 app.use("/api/chatbot", chatbotRoute);
 
 // ✅ connect to MongoDB (only once)
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => {
     console.error("❌ MongoDB connection error:", err.message);
@@ -35,12 +45,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/incidents", incidentRoutes);
 app.use("/api/sos", sosRoutes);
 
-
-
 // fallback
 app.get("/", (req, res) => res.send("🚀 Safety backend is up"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-
-
